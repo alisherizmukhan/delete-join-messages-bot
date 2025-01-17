788acad76e1e73117f842d2f13bcc1e5b642ac3a
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2025-01-17 14:10:30 +0500

    v1.1

--- api/bot.py
+++ api/bot.py
@@ -1,13 +1,9 @@
 from flask import Flask, request
 import telegram
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Telegram bot token
 TELEGRAM_BOT_TOKEN = "7621067756:AAGBJl_QSppcyQJx6hL88WnUcVBQvxNGlU0"
-
-# Initialize the Telegram bot
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
 @app.route("/api/webhook", methods=["POST"])
@@ -15,12 +11,15 @@ def webhook():
     # Parse the incoming update
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    # Check if it's a message and if it contains a "new_chat_member" field
+    # Check for new chat members and delete join messages
     if update.message and (update.message.new_chat_members or update.message.group_chat_created):
-        # Delete the message
         try:
             bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
         except telegram.error.TelegramError as e:
             print(f"Error deleting message: {e}")
 
     return "ok", 200
+
+@app.route("/api/webhook", methods=["GET"])
+def webhook_info():
+    return "This endpoint is for POST requests only.", 405
